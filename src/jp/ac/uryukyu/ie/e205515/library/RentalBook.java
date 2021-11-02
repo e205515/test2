70169c63d6f48bde8ca4d8eaebb5cb3fd089281b
TOMOKI CHINEN <e205515@ie.u-ryukyu.ac.jp>
2021-11-02 09:41:53 +0900

    1st commit

---
+++ src/jp/ac/uryukyu/ie/e205515/library/RentalBook.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e205515.library;
+
+public class RentalBook {
+    // フィールド（クラス変数）
+    public String title;
+    public int barcode;
+
+    // コンストラクタ
+    public RentalBook(String _title, int _barcode){
+        this.title = _title;
+        this.barcode = _barcode;
+    }
+
+    // staticなしメソッド例
+    public void printSummary(){
+        System.out.println("title =" + this.title + ", barcode = " + this.barcode);
+    }
+}
+
